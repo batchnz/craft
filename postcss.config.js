6380141828844387423e3e07e3584fa193251743
Alex Hatzipanis <alex@batch.nz>
2019-03-22 23:36:16 +1300

    Improve formatting

--- postcss.config.js
+++ postcss.config.js
@@ -1,16 +1,14 @@
 module.exports = {
-    plugins: [
-        require('postcss-import')({
-            plugins: [
-                require('stylelint')
-            ]
-        }),
-        require('tailwindcss')('./tailwind.js'),
-        require('postcss-preset-env')({
-            autoprefixer: { grid: true },
-            features: {
-                'nesting-rules': true
-            }
-        })
-    ]
+  plugins: [
+    require("postcss-import")({
+      plugins: [require("stylelint")]
+    }),
+    require("tailwindcss")("./tailwind.js"),
+    require("postcss-preset-env")({
+      autoprefixer: { grid: true },
+      features: {
+        "nesting-rules": true
+      }
+    })
+  ]
 };
