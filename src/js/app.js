0deca848ba85bea66ccda6ce6f723c0bdf9cac0b
Jude Reid <jude@batch.nz>
2020-09-14 15:07:13 +1200

    Adds layout, adds example component to index

--- src/js/app.js
+++ src/js/app.js
@@ -1,29 +1,36 @@
 /* eslint-disable no-unused-vars */
-import styles from '../css/app.css';
+import styles from "../css/app.css";
 /* eslint-enable no-unused-vars */
 
 const main = async () => {
   // Async load dependencies
-  const { default: Vue } = await import(/* webpackChunkName: 'vue' */ 'vue');
-  const { default: axios } = await import(/* webpackChunkName: 'axios' */ 'axios');
+  const { default: Vue } = await import(/* webpackChunkName: "vue" */ "vue");
+
+  const { default: axios } = await import(
+    /* webpackChunkName: "axios" */ "axios"
+  );
 
   return {
     Vue,
-    axios,
+    axios
   };
 };
 
 // Execute async function
-main().then((components) => {
+main().then(components => {
   const { Vue, axios } = components;
 
   // Add a global instance of axios to Vue
-  axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+  axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
   Vue.prototype.$axios = axios;
 
   /* eslint-disable no-unused-vars */
   const vm = new Vue({
-    el: '#app',
+    el: "#app",
+    components: {
+      HelloWorld: () =>
+        import(/* webpackChunkName: "HelloWorld" */ "../vue/HelloWorld.vue")
+    }
   });
   /* eslint-enable no-unused-vars */
 });
